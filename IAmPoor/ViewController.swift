46274f487f42d9ad4f1be2cea05f24703f9a4cd6
Deepak Rout <der9078@nyp.org>
2020-06-12 23:02:02 -0400

    Initial Commit

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Deepak Kumar Rout on 6/12/20.
+//  Copyright © 2020 Deepak Kumar Rout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
